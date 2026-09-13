c61d91c3d9e5d2d856eef4d0a582fb033927f3ef
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-09-14 05:16:29 +0600

    add

---
+++ CourierSystem/models/country.model.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('/config/database');
+
+const Country = sequelize.define('Country', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    code: {
+        type: DataTypes.STRING(3),
+        allowNull: true,
+        unique: true
+    },
+    phoneCode: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    active: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+    }
+}, {
+    tableName: 'countries',
+    timestamps: true
+});
+
+module.exports = Country;
